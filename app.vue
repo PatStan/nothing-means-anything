28aaf52dd0e517f8b6a1398b066aef07507e45fb
Patrick Stanton <patrickstanton50@gmail.com>
2024-01-26 23:35:43 +0000

    landing page

--- app.vue
+++ app.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="h-screen bg-black">
-    <div class="flex items-center justify-center pt-8">
-      <img src="assets/images/logo.png" height="515" width="447"/>
-    </div>
-    <h1 class="text-3xl text-white font-bold w-full text-center mt-8">
-      hi charli and josh :)
-    </h1>
+  <div class="flex items-center justify-center pt-8">
+    <img src="assets/images/logo.png" height="515" width="447"/>
   </div>
+    <h1 class="text-3xl w-full text-center mt-20">
+      Coming Soon
+    </h1>
 </template>
 <script setup lang="ts">
 </script>
