8b2fb3e6019b3e9ddc191c6d1fd7fccd782359a2
Patrick Stanton <patrickstanton50@gmail.com>
2024-01-26 23:44:54 +0000

    .editorconfig

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex items-center justify-center pt-8">
-    <img src="assets/images/logo.png" height="515" width="447"/>
-  </div>
+    <div class="flex items-center justify-center pt-8">
+        <img src="assets/images/logo.png" height="515" width="447"/>
+    </div>
     <h1 class="text-3xl w-full text-center mt-20">
-      Coming Soon
+        Coming Soon
     </h1>
 </template>
 <script setup lang="ts">
