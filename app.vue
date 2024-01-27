a04fac661d825283053658ff96885d10d43b0286
Patrick Stanton <patrickstanton50@gmail.com>
2024-01-27 00:06:46 +0000

    films page

--- app.vue
+++ app.vue
@@ -1,10 +1,4 @@
 <template>
-    <div class="flex items-center justify-center pt-8">
-        <img src="assets/images/logo.png" height="515" width="447"/>
-    </div>
-    <h1 class="text-3xl w-full text-center mt-20">
-        Coming Soon
-    </h1>
+    <NuxtPage>
+    </NuxtPage>
 </template>
-<script setup lang="ts">
-</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center justify-center pt-8">
+        <img src="assets/images/logo.png" height="515" width="447"/>
+    </div>
+    <h1 class="text-3xl w-full text-center mt-20">
+        Coming Soon
+    </h1>
+    <div class="flex items-center justify-center pt-8">
+        <NuxtLink to="/films">
+            Films
+        </NuxtLink>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
