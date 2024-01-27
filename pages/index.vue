01c9cbfb61498316607a938af5fb327fa80b8018
Patrick Stanton <patrickstanton50@gmail.com>
2024-01-27 00:15:08 +0000

    removed page

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,6 @@
     <h1 class="text-3xl w-full text-center mt-20">
         Coming Soon
     </h1>
-    <div class="flex items-center justify-center pt-8">
-        <NuxtLink to="/films">
-            Films
-        </NuxtLink>
-    </div>
 </template>
 <script setup lang="ts">
 </script>
---
+++ pages/novels.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <h1 class="text-3xl w-full text-center mt-20">
+        This is a page for novels
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
