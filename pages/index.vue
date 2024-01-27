3078027c164dae196db6942663a79ba20ba69a56
Patrick Stanton <patrickstanton50@gmail.com>
2024-01-27 00:19:34 +0000

    alt tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center pt-8">
-        <img src="assets/images/logo.png" height="515" width="447" />
+        <img src="assets/images/logo.png" height="515" width="447" alt="Nothing Means Anything Logo" />
     </div>
     <h1 class="text-3xl w-full text-center mt-20">Coming Soon</h1>
 </template>
