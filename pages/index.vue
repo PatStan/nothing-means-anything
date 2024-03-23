b857e1f7ec9fefafe08bcaa983345b8baa100f6d
Patrick Stanton <patrickstanton50@gmail.com>
2024-03-23 22:20:18 +0000

    home page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,33 @@
 <template>
-    <div class="flex items-center justify-center pt-8">
-        <img src="assets/images/logo.png" height="515" width="447" alt="Nothing Means Anything Logo" />
+    <div class="grid grid-cols-2 grid-rows-1 gap-0 w-screen h-screen p-20 m-auto">
+        <div class="w-full">
+            <div class="flex p-24 pl-6 pt-16">
+                <img src="assets/images/logo.png" height="515" width="447" alt="Nothing Means Anything Logo" />
+            </div>
+        </div>
+        <div>
+            <div class="grid grid-row-5 flex-nowrap m-auto h-full self-center">
+                <span class="h-full mb-20"></span>
+                <p class="text-lg pt-12 ml-24 text-right">
+                    Charlie Stanton & Joshua De Marco are Writer-Directors from East London working under the banner of
+                    Nothing Means Anything. <br />
+                    <br />
+                    When not listening or watching the world we should be in fail to exist, they usually write some
+                    shiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit to do with society, human emotions, cosmic thought
+                    exploration and bridge the gap between rich and poor in a way that yuppies and politicians alike can
+                    only dream of.
+                </p>
+                <p class="text-right text-2xl text-gray-500">CAPTURE THE INEFFABLE.</p>
+                <div class="text-right justify-end mt-8">
+                    <button class="bg-transparent py-1 px-6 border border-white">
+                        THEWRITERS@NOTHINGMEANSANYTHING.CO.UK
+                    </button>
+                </div>
+            </div>
+            <p class="text-xs align-bottom text-gray-600 text-right justify-end mt-10">
+                © Nothing Means Anything. Ltd..
+            </p>
+        </div>
     </div>
-    <h1 class="text-3xl w-full text-center mt-16">Coming Soon</h1>
-    <h1 class="text-xl w-full text-center pt-10">thewriters@nothingmeansanything.co.uk</h1>
 </template>
 <script setup lang="ts"></script>
