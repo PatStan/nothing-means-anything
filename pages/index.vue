4cba718276b4c2493b683fc9cc90cdbb84df860b
Patrick Stanton <patrickstanton50@gmail.com>
2024-03-23 11:07:13 +0000

    added email to coming soon index

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
     <div class="flex items-center justify-center pt-8">
         <img src="assets/images/logo.png" height="515" width="447" alt="Nothing Means Anything Logo" />
     </div>
-    <h1 class="text-3xl w-full text-center mt-20">Coming Soon</h1>
+    <h1 class="text-3xl w-full text-center mt-16">Coming Soon</h1>
+    <h1 class="text-xl w-full text-center pt-10">thewriters@nothingmeansanything.co.uk</h1>
 </template>
 <script setup lang="ts"></script>
