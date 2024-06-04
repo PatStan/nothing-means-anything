c0ba5a5ecc8eebce819ab53adb2e66c84027a883
Patrick Stanton <patrickstanton50@gmail.com>
2024-06-04 16:25:13 +0100

    complete

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <div
-        class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 gap-0 w-full max-h-svh md:h-screen p-4 md:p-12 m-auto overflow-auto"
+        class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 gap-0 w-full max-h-svh md:h-screen p-4 xl:p-16 m-auto overflow-auto"
     >
-        <div class="w-full bg-amber-300">
-            <div class="flex justify-center h-5/6 items-center m-4">
+        <div class="w-full">
+            <div class="flex justify-center h-5/6 items-center m-4 md:ml-6">
                 <img
                     class="object-scale-down w-full h-full mt-4 md:pt-10 md:pr-4"
                     src="assets/images/logo.png"
@@ -16,71 +16,37 @@
                 />
             </div>
         </div>
-        <div class="w-full flex flex-col h-full bg-amber-900">
-            <div class="bg-green-400 basis-2/5 p-2 pl-6">
-                <p class="text-sm text-right">
+        <div class="w-full flex flex-col h-full xl:px-2">
+            <div class="basis-2/5 md:basis-3/5 xl:basis-4/5 p-2 pl-6 xl:pl-34 xl:mt-40 md:place-content-end">
+                <p class="text-sm md:text-lg text-right">
                     Charlie Stanton & Joshua De Marco are <span class="inline-block">Writer-Directors</span> from East
                     London working under the banner of <span class="inline-block">Nothing Means Anything.</span>
                 </p>
-                <p class="text-sm text-right mt-4">
+                <p class="text-sm md:text-lg text-right mt-4">
                     When not listening or watching the world we should be in fail to exist, they usually write some
                     shiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit to do with society, human emotions, cosmic thought
                     exploration and bridge the gap between rich and poor in a way that yuppies and politicians alike can
                     only dream of.
                 </p>
             </div>
-            <div class="bg-green-500 basis-1/5 p-2 pt-6">
+            <div class="basis-1/5 md:basis-1/12 xl:basis-1/5 xl:pt-12 p-2 pt-6 md:justify-end md:place-content-center">
                 <p class="text-right text-2xl text-gray-500">CAPTURE THE INEFFABLE.</p>
             </div>
-            <div class="bg-green-600 basis-1/5 justify-center items-end">
-                <div class="flex justify-end p-2 pt-6">
-                    <p class="text-right text-xs bg-transparent py-1 px-1 border border-white">
+            <div class="basis-1/5 md:basis-1/12 justify-center items-end">
+                <div class="flex justify-end p-2 pt-6 md:pt-10 xl:mt-8">
+                    <p
+                        class="text-right text-xs md:text-sm xl:text-base bg-transparent py-1 px-1 xl:px-6 border border-white"
+                    >
                         THEWRITERS@NOTHINGMEANSANYTHING.CO.UK
                     </p>
                 </div>
             </div>
-            <div class="bg-green-700 basis-1/5">
+            <div class="basis-1/5 md:basis-1/4 md:place-content-end">
                 <div class="flex justify-end pt-12">
                     <p class="text-xs text-gray-600 text-right">© Nothing Means Anything. Ltd..</p>
                 </div>
             </div>
         </div>
     </div>
-    <!--    <div class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 gap-0 w-screen h-screen p-20 m-auto">-->
-    <!--        <div class="w-full">-->
-    <!--            <div class="flex justify-center sm:px-12 md:px-8 md:h-screen">-->
-    <!--                <img-->
-    <!--                    class="object-scale-down max-h-full m-auto pb-36"-->
-    <!--                    src="assets/images/logo.png"-->
-    <!--                    height="515"-->
-    <!--                    width="447"-->
-    <!--                    alt="Nothing Means Anything Logo"-->
-    <!--                />-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--        <div class="w-full">-->
-    <!--            <div class="grid grid-row-5 flex-nowrap m-auto h-full self-center justify-center">-->
-    <!--                <span class="h-full md:mb-20"></span>-->
-    <!--                        <p class="text-lg sm:text-sm pt-12 ml-24 text-right">-->
-    <!--                            Charlie Stanton & Joshua De Marco are Writer-Directors from East London working under the banner of-->
-    <!--                            Nothing Means Anything. <br />-->
-    <!--                            <br />-->
-    <!--                            When not listening or watching the world we should be in fail to exist, they usually write some-->
-    <!--                            shiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit to do with society, human emotions, cosmic thought-->
-    <!--                            exploration and bridge the gap between rich and poor in a way that yuppies and politicians alike can-->
-    <!--                            only dream of.-->
-    <!--                        </p>-->
-    <!--                        <p class="text-right text-2xl text-gray-500">CAPTURE THE INEFFABLE.</p>-->
-    <!--                            <div class="text-right justify-end mt-8">-->
-    <!--                                <button class="bg-transparent py-1 px-6 border border-white">-->
-    <!--                                    THEWRITERS@NOTHINGMEANSANYTHING.CO.UK-->
-    <!--                                </button>-->
-    <!--                            </div>-->
-    <!--            </div>-->
-    <!--                        <p class="text-xs align-bottom text-gray-600 text-right justify-end mt-10">-->
-    <!--                            © Nothing Means Anything. Ltd..-->
-    <!--                        </p>-->
-    <!--        </div>-->
-    <!--    </div>-->
 </template>
 <script setup lang="ts"></script>
